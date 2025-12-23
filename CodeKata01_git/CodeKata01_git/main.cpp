1b2a8f668725fca8d074ab43102c61821ac5db14
zzohyeyul <zzohyeyul@gmail.com>
2025-12-23 09:05:44 +0900

    CodeKata02

--- CodeKata01_git/CodeKata01_git/main.cpp
+++ CodeKata01_git/CodeKata01_git/main.cpp
@@ -8,4 +8,15 @@ int solution(int num1, int num2)
     int answer = num1 - num2;
 
     return answer;
-}
+} // 12.22 두 수의 차
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = num1 * num2;
+    return answer;
+} // 12.23 두 수의 곱
