5595ddf251d5cc8eb3adcbaf5e6d5d681a163352
zzohyeyul <zzohyeyul@gmail.com>
2025-12-29 10:29:37 +0900

    CodeKata

--- CodeKata01_git/CodeKata01_git/main.cpp
+++ CodeKata01_git/CodeKata01_git/main.cpp
@@ -56,3 +56,13 @@ int solution(int num1, int num2) {
         return -1;
     }
 } // 12.26 숫자 비교하기
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    return num1 + num2;
+} // 12.29 두 수의 합
