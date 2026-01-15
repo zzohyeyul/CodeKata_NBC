d3ab4bf857714588ceac8a9611164cc1faafeb5b
zzohyeyul <zzohyeyul@gmail.com>
2026-01-15 09:40:10 +0900

    CodeKata

--- CodeKata01_git/CodeKata01_git/main.cpp
+++ CodeKata01_git/CodeKata01_git/main.cpp
@@ -287,3 +287,21 @@ int solution(string s) {
 
     return result * sign;
 } // 1.14 문자열을 정수로 바꾸기
+
+
+
+#include <cmath>
+using namespace std;
+
+long long solution(long long n) {
+    long long x = (long long)sqrt((long double)n);
+
+    while (x * x < n) x++;
+    while (x * x > n) x--;
+
+    if (x * x == n) {
+        long long y = x + 1;
+        return y * y;
+    }
+    return -1;
+} // 정수 제곱근 판별
