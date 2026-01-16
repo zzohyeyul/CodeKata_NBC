11195268529c4c7e91a37c33746df00515e8d3fd
zzohyeyul <zzohyeyul@gmail.com>
2026-01-16 09:04:42 +0900

    CodeKata

--- CodeKata01_git/CodeKata01_git/main.cpp
+++ CodeKata01_git/CodeKata01_git/main.cpp
@@ -304,4 +304,19 @@ long long solution(long long n) {
         return y * y;
     }
     return -1;
-} // 정수 제곱근 판별
+} // 1.15 정수 제곱근 판별 
+
+
+
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(long long n) {
+    string s = to_string(n);
+
+    sort(s.begin(), s.end(), greater<char>());
+
+    return stoll(s);
+} // 1.16 정수 내림차순으로 배치하기
